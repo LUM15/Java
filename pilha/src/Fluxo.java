993719f945010a46b0b571c4799f55e6e7d854a2
Luis Miguel <luismqsjk@gmail.com>
2022-10-12 19:20:37 -0300

    Conclusão: C04 - Checked e Unchecked

--- pilha/src/Fluxo.java
+++ pilha/src/Fluxo.java
@@ -4,32 +4,27 @@ public class Fluxo {
 	public static void main(String[] args) {
 		
 		System.out.println("Ini do main");
-		metodo1();
+		try {
+		    metodo1();
+		} catch(ArithmeticException | NullPointerException | MinhaExcecao ex) {
+		    String msg = ex.getMessage();
+		    System.out.println("Exception " + msg);
+		    ex.printStackTrace();
+		} 
 		System.out.println("Fim do main");
 	}
 	
-	private static void metodo1() {
+	private static void metodo1() throws MinhaExcecao{
 		System.out.println("Ini do metodo1");
-		//Executara esse bloco de codigo com cautela
-		try {
-			metodo2();
-		} catch (ArithmeticException | NullPointerException e){
-			System.out.println("Exception " + e.getMessage());
-			e.printStackTrace();
-		}
+		metodo2();
 		System.out.println("Fim do metodo1");	
 	}
 
-	private static void metodo2() {
+	private static void metodo2() throws MinhaExcecao {
 		System.out.println("Ini do metodo2");
-		for (int i = 1; i <= 5; i++) {
-			System.out.println(i);
-			int a = i / 0;
-//			Conta c = null;
-//			c.deposita();
-//			
-		}
-		System.out.println("Fim do metodo2");	
+		
+		throw new MinhaExcecao("Deu muito errado!");
+		
 	}
 	
 }
---
+++ pilha/src/MinhaExcecao.java
@@ -0,0 +1,10 @@
+
+public class MinhaExcecao extends Exception {
+
+	
+	public MinhaExcecao(String message) {
+		
+		super(message);
+	}
+	
+}
