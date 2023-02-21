daf334988f3dced1a2f146473f1d28564688a45d
Luis Miguel <luismqsjk@gmail.com>
2023-02-21 16:51:27 -0300

    Conclusão: Java e Java.Util C02A08 - Entendendo o array String args

--- baibank-herdado-conta/src/br/com/baibank/banco/teste/TesteArrayReferencias.java
+++ baibank-herdado-conta/src/br/com/baibank/banco/teste/TesteArrayReferencias.java
@@ -1,13 +1,15 @@
 package br.com.baibank.banco.teste;
 
 import br.com.baibank.banco.modelo.Cliente;
+import br.com.baibank.banco.modelo.Conta;
 import br.com.baibank.banco.modelo.ContaCorrente;
+import br.com.baibank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 		
-		ContaCorrente[] contasCorrentes = new ContaCorrente[5];
+		Conta[] contas = new Conta[5];
 		
 		Cliente cliente00 = new Cliente("Heloise Sandra da Rosa", "770.707.020-86");
 		Cliente cliente01 = new Cliente("Eloá Amanda Araújo", "861.415.648-01");
@@ -16,23 +18,23 @@ public class TesteArrayReferencias {
 		Cliente cliente04 = new Cliente("Stella Isadora da Silva", "817.984.388-28");
 		
 		ContaCorrente cc1 = new ContaCorrente(33, 798, cliente00);
-		contasCorrentes[0] = cc1;
+		contas[0] = cc1;
 		
 		ContaCorrente cc2 = new ContaCorrente(44, 845, cliente01);
-		contasCorrentes[1] = cc2;
+		contas[1] = cc2;
 		
-		ContaCorrente cc3 = new ContaCorrente(88, 946, cliente02);
-		contasCorrentes[2] = cc3;
+		ContaPoupanca cc3 = new ContaPoupanca(88, 946, cliente02);
+		contas[2] = cc3;
 		
 		ContaCorrente cc4 = new ContaCorrente(99, 156, cliente03);
-		contasCorrentes[3] = cc4;
+		contas[3] = cc4;
 		
 		ContaCorrente cc5 = new ContaCorrente(66, 374, cliente04);
-		contasCorrentes[4] = cc5;
+		contas[4] = cc5;
 		
-		for (int i = 0; i < contasCorrentes.length; i++) {
+		for (int i = 0; i < contas.length; i++) {
 			
-			System.out.println(contasCorrentes[i].toString() + "\n");
+			System.out.println(contas[i].toString() + "\n");
 		
 		}
 	}
